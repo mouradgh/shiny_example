a0b7893ac75c2f049ea18528466dc6e2c0f6f892
Mourad ELGHISSASSI <mouradelghissassi1992@gmail.com>
2015-09-28 01:06:12 +0200

    Update ui.r

--- ui.r
+++ ui.r
@@ -1,25 +1,121 @@
-shinyUI(bootstrapPage(
+#Charger la bibliotheque Shiny
+library(shiny)
 
-  selectInput(inputId = "n_breaks",
-              label = "Number of bins in histogram (approximate):",
-              choices = c(10, 20, 35, 50),
-              selected = 20),
+#Debut de l'interface graphique
+shinyUI(
+  #Barre de navigation
+  #Nom de l'application
+  navbarPage("PMV",
+              #Page d'identification (Login/Mot de pass)
+              tabPanel("Login",
+                       
+                       tags$h3('Entrez vos identifiants'),
+                       #Ligne horizontale
+                       tags$hr(), 
+                       #Champ du Login
+                       htmlOutput("uiLogin"),
+                       #Champ du mot de pass
+                       htmlOutput("pass")
+              ),
+              #Page de chargement du fichier données
+              tabPanel("Charger le fichier et choisir la variable à ",
+                       titlePanel("Charger le fichier"),
+                       sidebarLayout(
+                         sidebarPanel(
+                           #Button pour charger un fichier à partir de l'ordinateur
+                           fileInput('file1', 'Choisir le fichier de données',
+                                     #Types de fichiers acceptés
+                                     accept=c('text/csv', 
+                                              'text/comma-separated-values,text/plain', 
+                                              '.csv')),
+                           tags$hr(),
+                           #Case à cocher si le fichier contient un en-tête
+                           checkboxInput('header', 'En-tête', TRUE),
+                           #Button pour choisir le séparateur (Virgule, Point virgule ou tabulation)
+                           radioButtons('sep', 'Séparateur',
+                                        #Liste des choix
+                                        c(Virgule=',',
+                                          PointVirgule=';',
+                                          Tabulation='\t'),
+                                        #Selectionner point-virgule par défaut 
+                                        ';' ),
+                           
+                  
+#                            radioButtons('quote', 'Quote',
+#                                         c(None='',
+#                                           'Double Quote'='"',
+#                                           'Single Quote'="'"),
+#                                         '"'),
 
-  checkboxInput(inputId = "individual_obs",
-                label = strong("Show individual observations"),
-                value = FALSE),
+                           #Button pour choisir le séparateur décimal (point ou virgule)
+                           radioButtons('dec', 'Séparateur décimal',
+                                        c('Virgule'=",",
+                                          'Point'="."),
+                                        #Selectionner virgule par défaut
+                                        'Virgule'),
+                           tags$hr(),
+                           #Choix de la variable à éxpliquer
+                           htmlOutput("selectDependent"),
+                           #Slider pour le choix du degre de regression polynomiale
+                           sliderInput('deg', 'Degré maximum pour la regression polynomiale', 2, 4, 2, 1)
+                         ),
+                         mainPanel(
+                           tags$h4('Si vous avez des erreur, assurez vous du choix des options de chargement du fichiers: en-tête, séparateur, décimal ...'),
+                           
+                           #Affichage d'un résumé des données
+                           #verbatimTextOutput("summary"),
+                           
+                           #Affichage des premieres ligne du fichier de donnees
+                           tableOutput("view")
+                         )
+                       )
+              ),
+              #Page d'affichage des différentes régressions
+              tabPanel("Choix du meilleur modèle",
+                       #Adapter la page à la taille de l'écran
+                       fluidRow(
+                         column(3,
+                              #Button pour stocker le modèle choisi dans la base
+                              actionButton("action", "Envoyer à la base de données")
+                         ),
+                         #Choix du meilleur modèle de regression
+                         column(3, offset = 1,
+                                radioButtons('mtype', h3('Model type'),
+                                             c(Linear = 'linear',
+                                               Exponential ='exponential',
+                                               Logarithmic ='logarithmic',
+                                               Polynomial ='polinomial'),
+                                             ',')
+                         ),
+                         #Choix des variables explicatives
+                         column(4,
+                                htmlOutput("checkvariables"),
+                                verbatimTextOutput("value")
+                         )),
+                         tags$hr(),
+                         tags$h3('Regressions'),
+                         
+                         #Affichage du tableau contenant tous les modèles
+                         dataTableOutput("bigtable")
+                       
+              ),
+             
+             #Page d'affichage du graphe (de regression)nuage de points)
+             tabPanel("Nuage de points",
+                      plotOutput('plot'),
+                      hr(),
+                      fluidRow(
+                        column(4, offset = 1,
+                               #Choisir la variable 1
+                               htmlOutput("selectUIx")
+                              ),
+                        column(5, offset = 1,
+                               #Choisir la variable 2
+                               htmlOutput("selectUIy")
+                              ) 
+                        )
+                      )
+             )
+)
+  
 
-  checkboxInput(inputId = "density",
-                label = strong("Show density estimate"),
-                value = FALSE),
-
-  plotOutput(outputId = "main_plot", height = "300px"),
-
-  # Display this only if the density is shown
-  conditionalPanel(condition = "input.density == true",
-    sliderInput(inputId = "bw_adjust",
-                label = "Bandwidth adjustment:",
-                min = 0.2, max = 2, value = 1, step = 0.2)
-  )
-
-))
